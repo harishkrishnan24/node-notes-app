a5d85b689099a4a29421a682d14e1684167d1eb1
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-08-29 12:33:11 +0530

    Add note

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
-const getNotes = require("./notes");
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(getNotes());
+const { getNotes, addNote } = require("./notes");
+
+yargs.version("1.1.0");
+
+yargs.command({
+	command: "add",
+	describe: "Add a new note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: "string",
+		},
+		body: {
+			describe: "Note body",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
+		addNote(argv.title, argv.body);
+	},
+});
+
+yargs.command({
+	command: "remove",
+	describe: "Remove a note",
+	handler: function () {
+		console.log("Removing a note");
+	},
+});
+
+yargs.command({
+	command: "list",
+	describe: "List all Notes",
+	handler: function () {
+		console.log("Listing all Notes");
+	},
+});
+
+yargs.command({
+	command: "read",
+	describe: "Read a note",
+	handler: function () {
+		console.log("Reading a note");
+	},
+});
+
+yargs.parse();
